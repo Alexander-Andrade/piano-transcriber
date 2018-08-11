09bed3637444c017addaf1e3df281e331638979d
Alexander-Andrade <veimar.94@gmail.com>
2018-08-11 11:48:37 +0300

    nothing spetial

--- main.py
+++ main.py
@@ -4,17 +4,29 @@ import matplotlib.pyplot as plt
 import time
 from models.midi_model import MidiNotes
 
-y, sr = librosa.load("samples/  original.mp3", 44100)
+y, sr = librosa.load("samples/original.mp3", 44100)
 y_mono = librosa.to_mono(y)
 spectrum = librosa.core.stft(y_mono)
 
 midi_notes = MidiNotes("samples/midi.mid")
 
+#%%
+
 start = time.time()
 
-frames_timestamps = librosa.core.frames_to_time(range(0, spectrum.shape[1]), 44100)
-frames_timestamps = [frames_timestamp * 1000 for frames_timestamp in frames_timestamps]
-labels = [ midi_notes.notes_at(timestamp)  for timestamp in frames_timestamps]
+train_set_len = spectrum.shape[1]
+frames_timestamps = librosa.core.frames_to_time(np.arange(train_set_len), 44100)
+frames_timestamps *= 1000
+#frames_timestamps = [frames_timestamp * 1000 for frames_timestamp in frames_timestamps]
+#labels = [ midi_notes.notes_at(timestamp)  for timestamp in frames_timestamps]
 
+train = np.zeros((train_set_len, 88), dtype=np.bool)
+
+
+for i, timestamp in enumerate(frames_timestamps):
+    notes = midi_notes.notes_at(timestamp)
+    notes_indexes = notes - 21
+    np.put(train[i], notes, True)
+    
 end = time.time()
 elapced = end - start
