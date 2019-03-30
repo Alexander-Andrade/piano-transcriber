bec3cc5f8f957e9ef4be912de42c08da071b8289
Alexander-Andrade <veimar.94@gmail.com>
2019-03-30 23:09:01 +0300

    test automated extraction

--- correlate_train_data.py
+++ correlate_train_data.py
@@ -71,52 +71,54 @@ def stat(name, slice_length=512, corr_interval=(-30, 30)):
             print("- [{0}, {1}]".format(frame, key))
 
 
+def manual_look():
+    name = 'beethoven_hammerklavier_3'
+    spectrum = np.load("../datasets/features_{0}.npy".format(name))
+    roll = np.load("../datasets/labels_{0}.npy".format(name))
+
+    print("frames: {0}".format(spectrum.shape[0]))
+
+    start_frame = 12000
+    slice_length = 512
+    corr_interval = (-50, 30)
+
+    features_slice, labels_slice, shift, corr_best, corr = train_slice(spectrum, roll,
+                                                            start_frame=start_frame,
+                                                            slice_length=slice_length,
+                                                            corr_interval=corr_interval)
+    print("shift: {0}, corr: {1}".format(shift, corr_best))
+
+    best_shift = -3
+    labels_slice = shifted_slice(roll, start_frame + best_shift, start_frame + slice_length + best_shift)
+
+    shifts = np.arange(corr_interval[0], corr_interval[1])
+    extrema = argrelextrema(corr, np.greater)
+    print(list(zip(np.take(shifts, extrema[0]).tolist(), np.take(corr, extrema[0]).tolist())))
+
+    l_sum = (median_filter(labels_slice.sum(axis=1), size=3))
+    if l_sum.max() != l_sum.min():
+        l_sum = normalize(l_sum)
+
+
+    grid = plt.GridSpec(2, 6, bottom=0.04, top=0.98, left=0.02, right=0.98)
+
+    axes = plt.subplot(grid[:, 0])
+    axes.imshow(features_slice)
+
+    axes = plt.subplot(grid[:, 1])
+    axes.imshow(labels_slice)
+
+    axes = plt.subplot(grid[0, 2:])
+    axes.plot(normalize(features_slice.sum(axis=1)))
+    axes.plot(l_sum)
+
+    axes = plt.subplot(grid[1, 2:])
+    axes.plot(shifts, corr)
+
+    mng = plt.get_current_fig_manager()
+    mng.window.state('zoomed')
+
+    plt.show()
 if __name__ == "__main__":
-    stat('beethoven_hammerklavier_2')
-    # name = 'beethoven_hammerklavier_2'
-    # spectrum = np.load("../datasets/features_{0}.npy".format(name))
-    # roll = np.load("../datasets/labels_{0}.npy".format(name))
-    #
-    # print("frames: {0}".format(spectrum.shape[0]))
-    #
-    # start_frame = 6000
-    # slice_length = 512
-    # corr_interval = (-50, 30)
-    #
-    # features_slice, labels_slice, shift, corr_best, corr = train_slice(spectrum, roll,
-    #                                                         start_frame=start_frame,
-    #                                                         slice_length=slice_length,
-    #                                                         corr_interval=corr_interval)
-    # print("shift: {0}, corr: {1}".format(shift, corr_best))
-    #
-    # best_shift = -3
-    # labels_slice = shifted_slice(roll, start_frame + best_shift, start_frame + slice_length + best_shift)
-    #
-    # shifts = np.arange(corr_interval[0], corr_interval[1])
-    # extrema = argrelextrema(corr, np.greater)
-    # print(list(zip(np.take(shifts, extrema[0]).tolist(), np.take(corr, extrema[0]).tolist())))
-    #
-    # l_sum = (median_filter(labels_slice.sum(axis=1), size=3))
-    # if l_sum.max() != l_sum.min():
-    #     l_sum = normalize(l_sum)
-    #
-    #
-    # grid = plt.GridSpec(2, 6, bottom=0.04, top=0.98, left=0.02, right=0.98)
-    #
-    # axes = plt.subplot(grid[:, 0])
-    # axes.imshow(features_slice)
-    #
-    # axes = plt.subplot(grid[:, 1])
-    # axes.imshow(labels_slice)
-    #
-    # axes = plt.subplot(grid[0, 2:])
-    # axes.plot(normalize(features_slice.sum(axis=1)))
-    # axes.plot(l_sum)
-    #
-    # axes = plt.subplot(grid[1, 2:])
-    # axes.plot(shifts, corr)
-    #
-    # mng = plt.get_current_fig_manager()
-    # mng.window.state('zoomed')
-    #
-    # plt.show()
+    stat('beethoven_hammerklavier_3')
+    #manual_look()
